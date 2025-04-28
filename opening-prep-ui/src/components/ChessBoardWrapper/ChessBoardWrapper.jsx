dc76f7137f623ba53f98e1f4b6ffbd16f952d405
Prog-droid <Jacob.Connor@live.co.uk>
2025-04-28 11:04:22 +0100

    'websocket'

--- opening-prep-ui/src/components/ChessBoardWrapper/ChessBoardWrapper.jsx
+++ opening-prep-ui/src/components/ChessBoardWrapper/ChessBoardWrapper.jsx
@@ -1,14 +1,55 @@
+import React, {
+  useState,
+  useEffect
+} from 'react';
 import { Chessboard } from "react-chessboard";
 import "./chessboard.css";
 
 export const ChessBoardWrapper = () => {
+  const host = "ws://localhost:8001/";
+  const [fen, setFen] = useState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+  useEffect(() => {
+    const websocket = new WebSocket(host);
+
+    websocket.onopen = () => {
+        console.log('WebSocket is connected');
+    };
+
+    websocket.addEventListener("message", ({ data }) => {
+      console.log("event");
+      
+      const event = JSON.parse(data);
+      console.log(event);
+      
+      // do something with event
+      setFen(event["fen"]);
+      //setFen("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1");
+    }); 
+    
+    websocket.onclose = () => {
+        console.log('WebSocket is closed');
+    };
+
+    return () => {
+        <>
+          <div className="boardWrapper">
+            <Chessboard
+              className="board"
+              position={fen}
+            />
+          </div>
+        </>
+    };
+}, [fen]);
+  
   return (
     <>
       <div className="boardWrapper">
         <Chessboard
           className="board"
           position={
-            "rnbqkbnr/pppp2pp/4p3/4Pp2/3P4/8/PPP2PPP/RNBQKBNR b KQkq - 0 3"
+            fen
           }
         />
       </div>
